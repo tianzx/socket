8438f65fc1d717161f1765bc6a3e61a9f824203f
tianzx <tianzx@aliyun.com>
2016-07-19 20:45:40 +0800

    multi proc

--- 02/echoServer.c
+++ 02/echoServer.c
@@ -1,48 +1,63 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <errno.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 #include <string.h>
+
 #define ERR_EXIT(m) \
-	do \
-	{ \
-		perror(m); \
-		exit(EXIT_FAILURE); \
-	} while(0)
-	
-	
-int main(void){
+        do \
+        { \
+                perror(m); \
+                exit(EXIT_FAILURE); \
+        } while(0)
+
+int main(void)
+{
 	int listenfd;
-	if((listenfd = socket(PF_INET,SOCK_STREAM,IPPROTO_TCP))<0)
+	if ((listenfd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+/*	if ((listenfd = socket(PF_INET, SOCK_STREAM, 0)) < 0)*/
 		ERR_EXIT("socket");
-	struct sockaddr_in serveraddr;
-	memset(&serveraddr,0,sizeof(serveraddr));
-	serveraddr.sin_family = AF_INET;
-	serveraddr.sin_port = htons(5188);
-	/**serveradd.sin_addr.s_addr = inet_addr("127.0.0.0");**/
-	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	if(bind(listenfd,(struct sockaddr*)&serveraddr,sizeof(serveraddr))<0)
+
+	struct sockaddr_in servaddr;
+	memset(&servaddr, 0, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(5188);
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	/*servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");*/
+	/*inet_aton("127.0.0.1", &servaddr.sin_addr);*/
+
+	int on = 1;
+	if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on)) < 0)
+		ERR_EXIT("setsockopt");
+
+	if (bind(listenfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0)
 		ERR_EXIT("bind");
-	if(listen(listenfd,SOMAXCONN)<0)
+	if (listen(listenfd, SOMAXCONN) < 0)
 		ERR_EXIT("listen");
+
 	struct sockaddr_in peeraddr;
 	socklen_t peerlen = sizeof(peeraddr);
-	int conn = 0;
-	if((conn= accept(listenfd,(struct sockaddr*)&peeraddr,&peerlen))<0)
-		ERR_EXIT("accpet");
+	int conn;
+	if ((conn = accept(listenfd, (struct sockaddr*)&peeraddr, &peerlen)) < 0)
+		ERR_EXIT("accept");
+
+	printf("ip=%s port=%d\n", inet_ntoa(peeraddr.sin_addr), ntohs(peeraddr.sin_port));
+
 	char recvbuf[1024];
-	while(1){
-		memset(recvbuf,0,sizeof(recvbuf));
-		int ret = read(conn,recvbuf,sizeof(recvbuf));
-		fputs(recvbuf,stdout);
-		write(conn,recvbuf,ret);
+	while (1)
+	{
+		memset(recvbuf, 0, sizeof(recvbuf));
+		int ret = read(conn, recvbuf, sizeof(recvbuf));
+		fputs(recvbuf, stdout);
+		write(conn, recvbuf, ret);
 	}
 	close(conn);
 	close(listenfd);
+	
 	return 0;
-
 }
